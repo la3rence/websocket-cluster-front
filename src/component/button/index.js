c96085e0a464cee0b20f31cdb4f95b8ec66e0f27
Lawrence Li <lonor@live.com>
2021-04-16 16:53:24 +0800

    refactor: button component

--- src/component/button/index.js
+++ src/component/button/index.js
@@ -1,11 +1,7 @@
-import {Component} from 'react'
-
-export default class Button extends Component {
-    render() {
-        return (
-            <button {...this.props}
-                    style={{border: 'none', textDecoration: 'underline', color: '#c9d1d9', outline: 'none',}}>
-            </button>
-        )
-    }
+export default function Button(props) {
+    return (
+        <button {...props}
+                style={{border: 'none', textDecoration: 'underline', color: '#c9d1d9', outline: 'none',}}>
+        </button>
+    )
 }
